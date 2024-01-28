9b7a0d3ffef958931a23e8db6bf77521196d7766
Daniil Panfilov <danielpanf@gmail.com>
2024-01-28 16:22:50 +0100

    made it to ignore .idea files in .gitignore.

--- FileReader.py
+++ FileReader.py
@@ -1,4 +1,11 @@
 class FileReader:
-    """This class reads strings from a file and converts them
-    to a """
-    def __init__(self,fliepath, filename):
+    """This class reads strings from a file and stores
+    them into lists."""
+    def __init__(self, filename, filepath = str()):
+        self.filename = filename
+        self.filepath = filepath
+        self.data = []
+
+    def fileReader(self):
+        file = open(filepath+filename)
+
